8e794512d7bd8088dd5519b3cde4dcec9a7eb031
Michael Trew <mtrew2015@gmail.com>
2020-11-29 23:49:10 -0500

    clean up comments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,7 +3,6 @@ import Comment from './Comment';
 import './Comments.css';
 
 const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,7 +3,6 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
@@ -12,8 +11,6 @@ const Posts = (props) => {
            return <Post post={post} likePost={likePost} />
         
         })}
-      {/* Map through the posts array returning a Post component at each iteration */}
-      {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
 };
